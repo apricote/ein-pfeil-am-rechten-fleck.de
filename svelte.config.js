be06093f09b532b87323617b57184f81e2b97505
Julian Tölle <julian.toelle97@gmail.com>
2021-05-15 18:27:05 +0200

    update dependencies

---
+++ svelte.config.js
@@ -0,0 +1,22 @@
+import sveltePreprocess from "svelte-preprocess";
+import vercel from "@sveltejs/adapter-vercel";
+
+/** @type {import("@sveltejs/kit").Config} */
+const config = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    sveltePreprocess({
+      postcss: true,
+    }),
+  ],
+
+  kit: {
+    adapter: vercel(),
+
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+  },
+};
+
+export default config;
