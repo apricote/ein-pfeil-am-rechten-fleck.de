6e6037fdac4b640b72dae459e8edb5e52ee292b1
Julian Tölle <julian.toelle97@gmail.com>
2023-10-08 16:05:59 +0200

    fix: typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,13 +27,13 @@
   />
   <Hero
     name="Tam"
-    description="Nur wenigen ist Tamriel, genannt Tam, überhaupt bekannt. Fängt sie jedoch an zu sprechen offenbart sich ein kluger Kopf. Als beste Freundin und stete Begleiterin von Beyron ist sie oftmals das gute Gewissen dieser Zweierbeziehung."
+    description="Nur wenigen ist Tamriel, genannt Tam, überhaupt bekannt. Fängt sie jedoch an zu sprechen, offenbart sich ein kluger Kopf. Als beste Freundin und stete Begleiterin von Beyron ist sie oftmals das gute Gewissen dieser Zweierbeziehung."
     image={TamImg}
     mirrored
   />
   <Hero
     name="Blue"
-    description="Ein langweiliges Leben führen wie ihre reichen Eltern? Nichts für Blue. Die weite Welt ist ein viel besserer Ort um erwachsen zu werden als ein staubiger Klassenraum."
+    description="Ein langweiliges Leben führen wie ihre reichen Eltern? Nichts für Blue. Die weite Welt ist ein viel besserer Ort, um erwachsen zu werden als ein staubiger Klassenraum."
     image={BlueImg}
   />
 </p>
