be600c37625c0c090b673edbb4c2b1355e076ae9
Julian Tölle <julian.toelle97@gmail.com>
2023-10-30 21:57:44 +0100

    chore: update dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,31 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
+    "plugin:svelte/recommended",
+    "plugin:svelte/prettier",
   ],
-  plugins: ["svelte3", "@typescript-eslint"],
-  ignorePatterns: ["*.cjs"],
-  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
-  settings: {
-    "svelte3/typescript": () => require("typescript"),
-  },
+  parser: "@typescript-eslint/parser",
   parserOptions: {
+    project: "./tsconfig.json",
+    extraFileExtensions: [".svelte"],
     sourceType: "module",
     ecmaVersion: 2020,
   },
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: ["*.cjs"],
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  ],
+
   env: {
     browser: true,
     es2017: true,
